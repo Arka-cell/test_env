d9c8479b7d2b38774b76feb7cbc184f0fcd5246d
Samir Ahmane <Samirahmane.trading@gmail.com>
2025-06-22 18:35:58 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return jsonify(
+        {
+            "app_name": os.getenv("APP_NAME", "Unknown App"),
+            "app_version": os.getenv("APP_VERSION", "0.0.0"),
+            "deploy_region": os.getenv("DEPLOY_REGION", "unknown-region"),
+        }
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
